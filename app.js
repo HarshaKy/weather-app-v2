caa7e8f59c1a22fb3a44d4de051121bf98a3b63a
HarshaKy <harshaky6573@gmail.com>
2019-03-01 22:21:17 +0530

    messing around with data from darksky

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/8ddcf168b2f164ee9d3fffd5162a0be4/37.8267,-122.4233';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+request({ url: url, json: true  }, (error, response) => {
+  // console.log(response.body.currently);
+  console.log(`It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.` );
 });
