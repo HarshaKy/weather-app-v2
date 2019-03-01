fca5cca8dd0b7f349e0dda4f1747b521b3843b6a
HarshaKy <harshaky6573@gmail.com>
2019-03-01 23:06:26 +0530

    asdf

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/8ddcf168b2f164ee9d3fffd5162a0be4/37.8267,-122.4233?units=si';
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/yelahanka%20new%20town.json?access_token=pk.eyJ1IjoiaGFyc2hha3kiLCJhIjoiY2pzcWIydGY5MTY2bTQ5bXhzMnFzcm9odyJ9.Qx2o5afY2Omo3TmlEIHpOw&limit=1';
+var {url, geocodeUrl} = require('./keys/keys.js')
 
 request({ url: url, json: true  }, (error, response) => {
   // console.log(response.body.currently);
