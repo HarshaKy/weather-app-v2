f6b979689de0cdb8944a2a4bec0218c8abfd5ab8
HarshaKy <harshaky6573@gmail.com>
2019-03-01 23:34:55 +0530

    get lat and lng values from mapbox

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const request = require('request');
 
-var {url, geocodeUrl} = require('./keys/keys.js')
-
-request({ url: url, json: true  }, (error, response) => {
-  // console.log(response.body.currently);
-  console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.` );
-});
+var {url, geocodeUrl} = require('./keys/keys.js');
 
 request({ url: geocodeUrl, json: true }, (error, response) => {
-  console.log(response.body.features[0].geometry.coordinates);
+  var lat = response.body.features[0].center[0];
+  var lng = response.body.features[0].center[1];
+  console.log(lat, lng);
+});
+
+request({ url: url, json: true  }, (error, response) => {
+  console.log(response.body.currently);
+  console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.` );
 });
